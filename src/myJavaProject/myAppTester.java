90b8c51e08dc7f4e4ee1ee4e25e7dda02f9fa054
Chun-Wei Huang <huangc12@student.douglascollege.ca>
2020-06-09 18:19:08 -0700

    updated myTestMethod() method

--- src/myJavaProject/myAppTester.java
+++ src/myJavaProject/myAppTester.java
@@ -17,7 +17,7 @@ public class myAppTester {
 	  reverseMethod(n);
 	  System.out.println();
 
-	  // add a line below to call your myTestMethod from here  then remove the block comment symbols  
+	  myTestMethod();
 
 	}
 	
@@ -31,17 +31,14 @@ public class myAppTester {
 	           System.out.print(num % 10);
 	           reverseMethod(num/10);     // recursive call
 	       }
-	   }
+	}
 
 
-	   /* 
-	       add your test method in this space then remove the block comment symbols
-	       public static void myTestMethod(...){
-
-            
-
-		   }
+	   
+	public static void myTestMethod(){
+		System.out.println("This method is created by Chun-Wei Huang for practicing"); 
+	}
 	 
-	   */
+	 
 
 }
